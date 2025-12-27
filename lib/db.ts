6a4cff6a189dd3620877936ffa4dcf42e9a397a4
rachit-k12 <rachit271105@gmail.com>
2025-12-27 21:55:36 +0530

    Refactor database connection to use environment variable for DATABASE_URL, enhancing security and configurability.

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,5 @@
 import { neon } from '@neondatabase/serverless';
 
-const sql = neon(
-  'postgresql://neondb_owner:npg_yNjh05pdSPFw@ep-ancient-cell-a4czzhlx-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require'
-);
+const sql = neon(process.env.DATABASE_URL!);
 
 export { sql };
